fc451353c29b4ca0c569c92f4f21aa3dd5caf90f
Jeremy Duncan <fbcjeremy@gmail.com>
2024-01-28 19:33:56 -0500

    heroku update2

--- index.js
+++ index.js
@@ -8,18 +8,19 @@ uuid = require('uuid');
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
+app.use(cors());
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-  app.use(cors({
-    origin: (origin, callback) => {
-      if(!origin) return callback(null, true);
-      if(allowedOrigins.indexOf(origin) === -1){
-        let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-        return callback(new Error(message ), false);
-      }
-      return callback(null, true);
-    }
-  }));
+// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+//   app.use(cors({
+//     origin: (origin, callback) => {
+//       if(!origin) return callback(null, true);
+//       if(allowedOrigins.indexOf(origin) === -1){
+//         let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+//         return callback(new Error(message ), false);
+//       }
+//       return callback(null, true);
+//     }
+//   }));
 
 const mongoose = require('mongoose');
 const Models = require('./models.js');
