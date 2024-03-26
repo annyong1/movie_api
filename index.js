b9c95e4aceb3c290708b74154f5b8d92ebb5a4c0
Jeremy Duncan <fbcjeremy@gmail.com>
2024-03-25 21:00:25 -0400

    endless fix 8

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect( process.env.CONNECTION_URI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express');
 const app = express();
