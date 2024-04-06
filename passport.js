5664334e897f2d760cebf64423b78c4afed19e0d
Jeremy Duncan <fbcjeremy@gmail.com>
2024-04-06 09:44:54 -0400

    endlesss fix 29

--- passport.js
+++ passport.js
@@ -23,7 +23,7 @@ passport.use(
                         message: 'Incorrect username or password.',
                     });
                 }
-                if (!user.validatePassord(password)) {
+                if (!user.validatePassword(password)) {
                     console.log('incorrect password');
                     return callback(null, false, { message: 'Incorrect password. '});
                 }
