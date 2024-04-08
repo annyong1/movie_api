d268d1874ac6febd53dd5116a043ee415eff3c0d
Jeremy Duncan <fbcjeremy@gmail.com>
2024-04-08 19:36:59 -0400

    last update???

--- index.js
+++ index.js
@@ -316,8 +316,6 @@ app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), (req
 
 //READ
 
-//app.get('/movies/genre/:genreName', (req, res) => {
-
 app.get('/movies/genre/:genreName', passport.authenticate('jwt', { session: false }), (req, res) => {
   
   const genreName = req.params.genreName;
@@ -340,18 +338,26 @@ app.get('/movies/genre/:genreName', passport.authenticate('jwt', { session: fals
 
 //READ
 
-app.get('/movies/director/:directorName', (req, res) => {
-  const {directorName} = req.params;
-  const director = movies.find( movie => movie.director.name === directorName ).director;
-
-  if (director) {
-    res.status(200).json(director);
-  } else {
-    res.status(400).send('no such director')
-  }
-
-})
+app.get('/movies/director/:directorName', passport.authenticate('jwt', { session: false }), (req, res) => {
   
+  const directorName = req.params.directorName;
+
+   console.log("Director requested:", req.params.directorName);
+  
+  Movies.findOne({ "Director.Name": directorName })
+    .then((movie) => {
+      if (!movie) {
+        return res.status(404).send('Error: ' + directorName + ' was not found');
+      }
+      res.status(200).json(movie.Director);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+  }
+);
+
 app.get('/documentation', (req, res) => {                  
   res.sendFile('public/documentation.html', { root: __dirname });
 });
