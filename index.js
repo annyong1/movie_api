a272a0ff5ba4a5a3ec59221df16f93f02c0118c9
Jeremy Duncan <fbcjeremy@gmail.com>
2024-04-20 10:07:04 -0400

    polishing

--- index.js
+++ index.js
@@ -25,18 +25,9 @@ app.use(cors());
 const Movies = Models.Movie;
 const Users = Models.User;
 
-
-//const dbUrl = process.env.CONNECTION_URI;
 const port = process.env.PORT || 8080;
 
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-//require('dotenv').config();
-
-//const uri = process.env.CONNECTION_URI;
-
-//const connectionString = 'mongodb+srv://jduncan:guesswho@duncandb.c8hefkw.mongodb.net/?retryWrites=true&w=majority&appName=DuncanDB';
-
 
 let allowedOrigins = [];
   
@@ -54,11 +45,6 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//mongoose.connect('mongodb://127.0.0.1:27017/DuncanDB');
-
-
-
-//CREATE
 
 app.post('/users',
   [
@@ -98,24 +84,6 @@ app.post('/users',
     });
 });
 
-// //UPDATE
-
-// app.put('/users/:id', (req,res) => {
-//   const { id } = req.params;
-//   const updatedUser = req.body;
-
-//   let user = users.find( user => user.id == id );
-  
-//   if (user) {
-//     user.name = updatedUser.name;
-//     res.status(200).json(user);
-//     } else {
-//       res.status(400).send('no such user')
-//     }
-// })
-
-//CREATE
-
 app.post('/users/:id/:movieTitle', (req,res) => {
   const { id, movieTitle } = req.params;
   
@@ -128,21 +96,6 @@ app.post('/users/:id/:movieTitle', (req,res) => {
       res.status(400).send('no such user')                                                                                               
     }
 })  
-
-//DELETE
-
-// app.delete('/users/:id/:movieTitle', (req,res) => {
-//   const { id, movieTitle } = req.params;
-  
-// let user = users.find( user => user.id == id );
-  
-//   if (user) {
-//     user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
-//     res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);;
-//     } else {
-//       res.status(400).send('no such user')                                                                                               
-//     }
-// })  
 
 app.delete('/users/:id/:movieID', (req, res) => {
   const { id, movieID } = req.params;
@@ -163,8 +116,6 @@ app.delete('/users/:id/:movieID', (req, res) => {
   });
 });
 
-//Delete user - mongoose
-
 app.delete('/users/:Username', async (req, res) => {
   await Users.findOneAndDelete({ Username: req.params.Username })
     .then((user) => {
@@ -180,8 +131,6 @@ app.delete('/users/:Username', async (req, res) => {
   });
 });
 
-//DELETE
-
 app.delete('/users/:id', (req,res) => {
   const { id } = req.params;
   
@@ -196,10 +145,8 @@ app.delete('/users/:id', (req,res) => {
 })  
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!'); // Send a response to the client
+  res.send('Hello, world!');
 });
-
-//Get all users - mongooser
 
 app.get('/users', passport.authenticate('jwt', {session: false}), async (req, res) => {
   await Users.find()
@@ -212,8 +159,6 @@ app.get('/users', passport.authenticate('jwt', {session: false}), async (req, re
     });
 });
 
-//Get a user by username - mongoose
-
 app.get('/users/:Username', async (req, res) => {
   await Users.findOne({ Username: req.params.Username })
     .then((user) => {
@@ -224,27 +169,6 @@ app.get('/users/:Username', async (req, res) => {
       res.status(500).send('Error: ' + err);
     });
   });
-
-//Update user by username - mongoose
-
-// app.put('/users/:Username', async (req, res) => {
-//   await Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
-//     {
-//       Username: req.body.Username,
-//       Password: req.body.Password,
-//       Email: req.body.Email,
-//       Birthday: req.body.Birthday
-//     }
-//   },
-//   { new: true })
-//   .then((updatedUser) => {
-//     res.json(updatedUser); 
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//     res.status(500).send('Error: ' + err);
-//   })
-// });
 
 app.post('/users/:Username/movies/:MovieID', async (req,res) => {
   await Users.findOneAndUpdate({ Username: req.params.Username }, {
@@ -259,8 +183,6 @@ app.post('/users/:Username/movies/:MovieID', async (req,res) => {
     res.status(500).send('Error: ' + err);
   });
 });
-
-//JWT CONDITION ADDED
 
 app.put('/users/:Username', passport.authenticate('jwt', {
 session: false }), async (req, res) => {
@@ -286,8 +208,6 @@ session: false }), async (req, res) => {
     })
 });
 
-//Update list of favorite movies for a user - mongoose
-
 app.post('/users/:Username/movies/:movieTitle', async (req, res) => {
   await Users.findOneAndUpdate({ Username: req.params.Username }, {
     $push: { FavoriteMovies: req.params.movieTitle }
@@ -302,11 +222,6 @@ app.post('/users/:Username/movies/:movieTitle', async (req, res) => {
   });
 });
 
-// READ
-
-
-//READ or GET W/ JWT AUTHENTICATION
-
 app.get('/movies', passport.authenticate('jwt', {session: false}), async (req, res) => {
   await Movies.find()
     .then((movies) => {
@@ -317,8 +232,6 @@ app.get('/movies', passport.authenticate('jwt', {session: false}), async (req, r
       res.status(500).send('Error: ' + error);
     });
 });
-
-//READ
 
 app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.findOne({ Title: req.params.Title })
@@ -334,8 +247,6 @@ app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), (req
       });
     }
 );
-
-//READ
 
 app.get('/movies/genre/:genreName', passport.authenticate('jwt', { session: false }), (req, res) => {
   
@@ -356,8 +267,6 @@ app.get('/movies/genre/:genreName', passport.authenticate('jwt', { session: fals
     });
   }
 );
-
-//READ
 
 app.get('/movies/director/:directorName', passport.authenticate('jwt', { session: false }), (req, res) => {
   
