78cea262d234394bccd8cb08bb3e100da6a98b05
Jeremy Duncan <fbcjeremy@gmail.com>
2024-06-23 18:45:08 -0400

    /login attempt fix

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(
 			if (allowedOrigins.indexOf(origin) === -1) {
 				// If a specific origin isn't found on the list of allowed origins
 				let message =
-					'The CORS policy for this application doesn"t allow access from origin ' +
+					"The CORS policy for this application doesn't allow access from origin " +
 					origin;
 				return callback(new Error(message), false);
 			}
