577cc101a8c7c332da8a7101439afc87540305b9
Jeremy Duncan <fbcjeremy@gmail.com>
2024-06-22 20:21:14 -0400

    3.5 redo

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const Models = require('./models');
 const express = require('express');
 const app = express();
 
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://duncanflixdb-4ad2a1debcf7.herokuapp.com/'
+
+]
+
 let auth = require('./auth')(app);
 
 const bodyParser = require('body-parser');
@@ -233,7 +239,7 @@ app.get('/movies/:Title', passport.authenticate ('jwt', { session: false }), (re
       console.error(err);
       res.status(500).send('Error: ' + err);
     });
-}
+  }
 );
 
 app.get('/movies/genre/:genreName', passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -273,10 +279,32 @@ app.get('/movies/director/:directorName', passport.authenticate('jwt', { session
       console.error(err);
       res.status(500).send('Error: ' + err);
     });
-}
+  }
 );
 
 app.use(express.static("public"));
+
+app.post('/signup', (req, res) => {
+  const { Username, Passowrd } = req.body;
+  const userExists = users.find(user => user.Username === Username)
+  if(userExists) {
+    return res.status(400).json({message: 'User already exists'})
+  }
+  const newUser = { Username, Password };
+  users.push(newUser);
+  res.status(201).json({ message: 'User created successfully', user: newUser });
+});
+
+app.post('/login', (req, res) => {
+  const { Username, Password } = req.body;
+  const user = users.find(user => user.Username === Username && user.Password === Password);
+  if (user) {
+    const token = 'your_jwt_secret';
+    res.json({ user, token });
+  } else {
+    res.status(400).json({ message: 'Invalid username or password' }); 
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
