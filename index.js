10483d4b7d3b385712785795b9cb2dc4cce9ee1f
Jeremy Duncan <fbcjeremy@gmail.com>
2024-07-01 21:09:59 -0400

    remove cors

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//test
-
 require('dotenv').config();
 
 const passport = require('passport');
@@ -21,26 +19,6 @@ uuid = require('uuid');
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
-app.use(cors());
-
-// Allow any oragin to access app
-let allowedOrigins = "*";
-
-app.use(
-	cors({
-		origin: (origin, callback) => {
-			if (!origin) return callback(null, true);
-			if (allowedOrigins.indexOf(origin) === -1) {
-				// If a specific origin isn't found on the list of allowed origins
-				let message =
-					"The CORS policy for this application doesn't allow access from origin " +
-					origin;
-				return callback(new Error(message), false);
-			}
-			return callback(null, true);
-		},
-	})
-);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
