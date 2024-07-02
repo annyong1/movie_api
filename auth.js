9dacedbab6b63e9c66cdc9f51524a1737487e8b4
Jeremy Duncan <fbcjeremy@gmail.com>
2024-07-01 21:02:09 -0400

    auth.js change

--- auth.js
+++ auth.js
@@ -1,31 +1,26 @@
-const jwtSecret = 'your_jwt_secret';
-//has to be same key used in JWTStrategy
+const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
 
 const jwt = require('jsonwebtoken'),
-    passport = require('passport');
+	passport = require('passport');
 
 require('./passport');
-//local passport file
 
 let generateJWTToken = (user) => {
-    return jwt.sign(user, jwtSecret, {
-        subject: user.Username,
-        //username encoding in the JWT
-        expiresIn: '7d',
-        //token will expire in 7 days
-        algorithm: 'HS256'
-        //used to encode values of JWT
-    });
-}
+	return jwt.sign(user, jwtSecret, {
+		subject: user.Username, // This is the username you're encoding in the JWT
+		expiresIn: '7d', // This specifies that the token will expire in 7 days
+		algorithm: 'HS256', // This is the algorithm used to "sign" or encode the values of the JWT
+	});
+};
 
-/*POST login.*/
+/* /login POST request */
 module.exports = (router) => {
 	router.post('/login', (req, res) => {
 		passport.authenticate('local', { session: false }, (error, user, info) => {
 			if (error || !user) {
 				return res.status(400).json({
 					message: 'Something is not right',
-					user: user
+					user: user,
 				});
 			}
 			req.login(user, { session: false }, (error) => {
