2fcf71136cd808de4cbc4e61070f40aaabd63e1f
Jeremy Duncan <fbcjeremy@gmail.com>
2024-08-30 11:48:05 -0400

    edit user fix

--- index.js
+++ index.js
@@ -224,11 +224,12 @@ app.put('/users/:Username', passport.authenticate('jwt', {
   if (req.user.Username !== req.params.Username) {
     return res.status(400).send('Permission denied');
   }
+  let hashedPassword = Users.hashPassword(req.body.Password);
   await Users.findOneAndUpdate({ Username: req.params.Username }, {
     $set:
     {
       Username: req.body.Username,
-      Password: req.body.Password,
+      Password: hashedPassword,
       Email: req.body.Email,
       Birthday: req.body.Birthday
     }
